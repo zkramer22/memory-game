d1a44e62de308694a78514a861ca75ed8310e2c8
Zach Kramer <zachary.kramer@syneoshealth.com>
2019-02-05 16:20:44 -0500

    implement basic logic for memory game, styling for tile flipping effect

--- src/comp/Grid.js
+++ src/comp/Grid.js
@@ -3,12 +3,49 @@ import Tile from './Tile.js';
 import { TILES, shuffler } from '../TileData.js';
 
 class Grid extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tiles: shuffler(TILES),
+      comparing: false,
+      cardCheck: [],
+      matches: 0,
+    };
+  }
+
+  compareCards(card) {
+    this.state.cardCheck.push(card);
+
+    if (this.state.cardCheck.length === 2) {
+      if (this.state.cardCheck[0].props.word === this.state.cardCheck[1].props.word) {
+        console.log('match!');
+        setTimeout(() => {
+          this.setState({ cardCheck: [], matches: this.state.matches + 1 });
+        }, 1000);
+      }
+      else {
+        console.log('nope.');
+        setTimeout(() => {
+          this.state.cardCheck.forEach((card) => {
+            card.setState({ flipped: false })
+            this.setState({ cardCheck: [] });
+          })
+        }, 1000);
+        //
+        // need to cancel all click events for tiles during this
+        // 1000ms timeout
+        // 
+      }
+    }
+  }
+
   render() {
     return (
       <div className="grid-wrapper">
-        { shuffler(TILES).map((tile, i) => {
+        { this.state.tiles.map((tile, i) => {
           return (
-            <Tile key={ i } color={ tile.color } word={ tile.word }/>
+            <Tile key={ i } color={ tile.color } word={ tile.word }
+            compareCards={ this.compareCards.bind(this) } />
           );
         }) }
       </div>
--- src/comp/Tile.js
+++ src/comp/Tile.js
@@ -1,12 +1,35 @@
+import back from '../img/heart.svg';
 import React, { Component } from 'react';
 
 class Tile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { flipped: false };
+    this.flipCard = this.flipCard.bind(this);
+  }
+
+  flipCard(e) {
+    if (!this.state.flipped) {
+      this.setState({ flipped: true });
+    }
+    else {
+      this.setState({ flipped: false });
+    }
+    this.props.compareCards(this);
+  }
+
   render() {
     const { color, word } = this.props;
-    
+    const flipClass = this.state.flipped ? 'tile-wrapper flipped' : 'tile-wrapper';
+
     return (
-      <div className="tile-wrapper" style={{ backgroundColor: color }}>
-        <p>{ word }</p>
+      <div onClick={ this.flipCard } className={ flipClass }>
+        <div className="tile-back" style={{ backgroundColor: color }}>
+          <img src={ back } alt="card"/>
+        </div>
+        <div className="tile-front flipped">
+          <p>{ word }</p>
+        </div>
       </div>
     );
   }
