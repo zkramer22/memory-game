d71d25ad8397951779ef2c61e35f84cda945f7bc
Zach Kramer <zachary.kramer@syneoshealth.com>
2019-02-07 13:20:55 -0500

    Updates

---
+++ precache-manifest.c9cda5f1d259d76ea859bcb0d58ed440.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be942e464202276d8bc68d02b1ff982c",
+    "url": "/navicor-memory-game/static/media/heart.be942e46.svg"
+  },
+  {
+    "revision": "90a883c074ef3f8afa65",
+    "url": "/navicor-memory-game/static/js/runtime~main.90a883c0.js"
+  },
+  {
+    "revision": "15522a6c499177671588",
+    "url": "/navicor-memory-game/static/js/main.15522a6c.chunk.js"
+  },
+  {
+    "revision": "ed88d9409e2279918096",
+    "url": "/navicor-memory-game/static/js/1.ed88d940.chunk.js"
+  },
+  {
+    "revision": "15522a6c499177671588",
+    "url": "/navicor-memory-game/static/css/main.4421fed6.chunk.css"
+  },
+  {
+    "revision": "525cfc08317aa90058d72d46ac2ff305",
+    "url": "/navicor-memory-game/index.html"
+  }
+];
