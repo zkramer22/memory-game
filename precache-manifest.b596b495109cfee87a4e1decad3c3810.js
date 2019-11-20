a10b65c46d614efed09609c9a75812d65398979c
zach kramer <zkramer22@gmail.com>
2019-11-19 22:43:49 -0800

    Updates

---
+++ precache-manifest.b596b495109cfee87a4e1decad3c3810.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bbc58fa8f95e15992d07f30b9a714d1",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "b4a85ebafb0eee74aba3",
+    "url": "/memory-game/static/css/main.4aae6174.chunk.css"
+  },
+  {
+    "revision": "057869f68b75b3cbdae3",
+    "url": "/memory-game/static/js/2.c8920c41.chunk.js"
+  },
+  {
+    "revision": "b4a85ebafb0eee74aba3",
+    "url": "/memory-game/static/js/main.9bfe165a.chunk.js"
+  },
+  {
+    "revision": "3c6c1d3098f1f9b94d8d",
+    "url": "/memory-game/static/js/runtime-main.53e70fbf.js"
+  },
+  {
+    "revision": "3a7b73f977386054ee5a3ba13640a887",
+    "url": "/memory-game/static/media/diamond.3a7b73f9.png"
+  }
+]);
